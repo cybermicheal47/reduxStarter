e71160507b476b08c20f3af8d9b5727904b6bf7e
cybermicheal47 <65826184+cybermicheal47@users.noreply.github.com>
2023-11-04 00:06:15 -0700

    async thunk method for fetching api

--- redux-starter-main/src/index.js
+++ redux-starter-main/src/index.js
@@ -2,21 +2,25 @@ import store from "./store/configureStore";
 // import { addTask, removeTask, completedTask } from "./store/tasks";
 // import { addemployee } from "./store/Employee";
 import axios from "axios";
-import { getTasks } from "./store/tasks";
+import { getTasks, fetchTasks } from "./store/tasks";
 
-//calling api using function
-const gettingTasks = async () => {
-  //calling api
-  try {
-    const response = await axios.get("http://localhost:9001/api/tasks");
-    console.log(response);
-    //dispatching actions
-    store.dispatch(getTasks({ tasks: response.data }));
-  } catch (error) {
-    store.dispatch({ type: "SHOW_ERROR", payload: { error: error.message } });
-  }
-};
-gettingTasks();
+//calling using asyncthuck
+
+store.dispatch(fetchTasks());
+
+// //calling api using function
+// const gettingTasks = async () => {
+//   //calling api
+//   try {
+//     const response = await axios.get("http://localhost:9001/api/tasks");
+//     console.log(response);
+//     //dispatching actions
+//     store.dispatch(getTasks({ tasks: response.data }));
+//   } catch (error) {
+//     store.dispatch({ type: "SHOW_ERROR", payload: { error: error.message } });
+//   }
+// };
+// gettingTasks();
 
 // store.dispatch(addTask({ task: "task1", completed: true }));
 // store.dispatch(addTask({ task: "task2", completed: true }));
--- redux-starter-main/src/store/tasks.js
+++ redux-starter-main/src/store/tasks.js
@@ -1,28 +1,64 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../utils/http";
 let id = 0;
+const initialState = {
+  tasks: [],
+  loading: false,
+  error: null,
+};
+
+export const fetchTasks = createAsyncThunk(
+  "fetchTasks",
+  async (a, { rejectWithValue }) => {
+    try {
+      const response = await axios.get("/tasks");
+      return { tasks: response.data };
+    } catch (error) {
+      return rejectWithValue({ error: error.message });
+    }
+  }
+);
+
 const taskSlice = createSlice({
   name: "tasks",
-  initialState: [],
+  initialState: initialState,
   reducers: {
     //action : function
     getTasks: (state, action) => {
-      return action.payload.tasks;
+      state.tasks = action.payload.tasks;
     },
     addTask: (state, action) => {
-      state.push({
+      state.tasks.push({
         id: ++id,
         task: action.payload.task,
         completed: action.payload.completed,
       });
     },
     removeTask: (state, action) => {
-      const index = state.findIndex((task) => task.id === action.payload.id);
-      state.splice(index, 1);
+      const index = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      state.tasks.splice(index, 1);
     },
     completedTask: (state, action) => {
-      const index = state.findIndex((task) => task.id === action.payload.id);
-      state[index].completed = true;
+      const index = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      state.tasks[index].completed = true;
+    },
+  },
+  extraReducers: {
+    [fetchTasks.pending]: (state, action) => {
+      state.loading = true;
+    },
+
+    [fetchTasks.fulfilled]: (state, action) => {
+      state.tasks = action.payload.tasks;
+      state.loading = false;
+    },
+    [fetchTasks.rejected]: (state, action) => {
+      state.error = action.payload.error;
+      state.loading = false;
     },
   },
 });
